dde5a4da8c0ff32ea4626ce344a264508b27fc1a
swaff-y <kyle@swaff.id.au>
2021-05-24 06:49:43 +1000

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,26 @@ Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   root :to => 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/user_login' => 'users#user_login'
+
+
+  get '/projects-user/:user' => 'projects#user_projects'
+  get '/user-one/:user' => 'users#user_one'
+  get '/project-read/:project_id' => 'projects#project_read'
+  delete '/project-delete/:project_id' => 'projects#project_delete'
+  get '/project-user/:project_id' => 'projects#user_project'
+  get '/project-user-status/:project_id/:status' => 'projects#user_project_status'
+  post '/project-priority-update/:project_id/:priority' => 'projects#project_priority_update'
+  post '/project-create/:user_id' => 'projects#create_project'
+  post '/task-create/:project_id' => 'tasks#create_task'
+  delete '/task-delete/:task_id' => 'tasks#delete_task'
+  get '/task-read/:task_id' => 'tasks#read_task'
+  post '/task-update/:task_id' => 'tasks#update_task'
+  post '/project-update/:project_id' => 'projects#update_project'
+
+  post '/task-priority-update' => 'tasks#task_priority_update'
+
+  post '/note-create/:task_id/:user_id' => 'notes#create_note'
+
 end
