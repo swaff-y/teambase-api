919b26031e3a13ce9055a2897eddc77cd5bce089
swaff-y <kyle@swaff.id.au>
2021-05-24 06:48:06 +1000

    update projects and tasks controller with authentication

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user
   before_action :set_project, only: [:show, :update, :destroy]
-  # before_action :authenticate_user
+
 
   # GET /projects
   def index
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user
   before_action :set_task, only: [:show, :update, :destroy]
-  # before_action :authenticate_user
+
 
   # GET /tasks
   def index
